ece60b4fdf8a52302ce533d1ca3c406cc3c6ea22
SepVeneto <sepveneto@qq.com>
2024-11-18 08:51:14 +0800

    chore: debug

--- packages/server/server/api/auth.ts
+++ packages/server/server/api/auth.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 export default defineEventHandler(async (evt) => {
 
   const res = await readBody(evt)
+  console.log(res.pwd, process.env.PASSWORD)
   if (res.pwd !== process.env.PASSWORD) {
     return
   }
